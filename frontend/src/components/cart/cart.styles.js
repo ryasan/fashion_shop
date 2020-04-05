d09c8ce66090963e3327869cbe0dbaa8e206bf20
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-04-04 20:21:58 -0700

    moving to public repo

---
+++ frontend/src/components/cart/cart.styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const Cart = styled.div``
+
+export default Cart
---
+++ frontend/src/components/cart/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Cart from './cart.styles'
+
+const CartComponent = () => {
+  return <Cart></Cart>
+}
+
+export default CartComponent
