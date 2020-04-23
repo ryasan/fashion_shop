555421658da3bd87f12ae9b8a64ce82dd5716a4d
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-04-23 00:45:30 -0700

    debug cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,11 +42,12 @@ server.express.use(async (req, res, next) => {
 const options = {
   cors: {
     credentials: true,
-    origin: [
-      process.env.NODE_ENV === 'development'
-        ? DEV_FRONTEND_URL
-        : PROD_FRONTEND_URL
-    ]
+    // origin: [
+    //   process.env.NODE_ENV === 'development'
+    //     ? DEV_FRONTEND_URL
+    //     : PROD_FRONTEND_URL
+    // ]
+    origin: '*'
   },
   endpoint: '/',
   playground: '/playground',
