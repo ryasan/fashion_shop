9790f2c0768ed0525c1216d127ba3cece637f989
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-04-26 20:51:07 -0700

    filtering works

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -1,4 +1,3 @@
-// const { intersection } = require('lodash')
 const { forwardTo } = require('prisma-binding')
 
 const Query = {
@@ -8,22 +7,17 @@ const Query = {
     return ctx.db.query.user({ where: { id: ctx.request.userId } }, info)
   },
   products: forwardTo('db'),
-  productsConnection: async (parent, { where }, ctx, info) => {
-    const products = await ctx.db.query.productsConnection({}, info)
-    console.log(products)
-    return products
-    // console.log(products)
-    // if (where.AND) {
-    //   const inputFilterValues = where.AND.map((filter) => {
-    //     return Object.values(filter)[0]
-    //   })
+  productsConnection: async (parent, { filters }, ctx, info) => {
+    if (filters.AND) {
+      const inputFilterValues = filters.AND.map((filter) => {
+        return Object.values(filter)[0]
+      })
+      if (inputFilterValues.every((v) => !v)) {
+        return ctx.db.query.productsConnection({}, info)
+      }
+    }
 
-    //   if (inputFilterValues.every((v) => !v)) {
-    //     console.log('-------->')
-    //   }
-    // }
-
-    // return ctx.db.query.productsConnection({ where }, info)
+    return ctx.db.query.productsConnection({ where: filters }, info)
   }
 }
 
