972267819d62f22e35c2f548be17beacb0dd3c2c
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-04-26 20:00:45 -0700

    commit

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -9,46 +9,22 @@ const Query = {
   },
   products: forwardTo('db'),
   productsConnection: async (parent, { where }, ctx, info) => {
-    if (where.AND) {
-      const inputFilterValues = where.AND.map((filter) => {
-        return Object.values(filter)[0]
-      })
+    const products = await ctx.db.query.productsConnection({}, info)
+    console.log(products)
+    return products
+    // console.log(products)
+    // if (where.AND) {
+    //   const inputFilterValues = where.AND.map((filter) => {
+    //     return Object.values(filter)[0]
+    //   })
 
-      if (inputFilterValues.every((v) => !v)) {
-        console.log('-------->')
-        return ctx.db.query.productsConnection({}, info)
-      }
-    }
+    //   if (inputFilterValues.every((v) => !v)) {
+    //     console.log('-------->')
+    //   }
+    // }
 
-    return ctx.db.query.productsConnection({ where }, info)
+    // return ctx.db.query.productsConnection({ where }, info)
   }
-  // productsConnection: async (parent, args, ctx, info) => {
-  //   const { sizeFilters } = args.filters
-  //   const oldConnection = await ctx.db.query.productsConnection({}, info)
-  //   if (!sizeFilters.length) return oldConnection
-
-  //   for (const p of oldConnection.edges) {
-  //     const product = p.node
-  //     const isAvailable = intersection(product.availableSizes, sizeFilters).length > 0 // prettier-ignore
-  //     const { __typename, id, ...newProduct } = product
-
-  //     await ctx.db.mutation.updateProduct({
-  //       where: { id },
-  //       data: {
-  //         ...newProduct,
-  //         isAvailable: isAvailable,
-  //         availableSizes: { set: newProduct.availableSizes }
-  //       }
-  //     })
-  //   }
-
-  //   const newConnection = await ctx.db.query.productsConnection(
-  //     { where: { isAvailable: true } },
-  //     info
-  //   )
-
-  //   return newConnection
-  // }
 }
 
 module.exports = Query
