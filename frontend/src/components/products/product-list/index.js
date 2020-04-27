a196579eea0d791a03d4c5f5ada3548774dece8b
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-04-27 13:43:30 -0700

    use button for seeding

--- frontend/src/components/products/product-list/index.js
+++ frontend/src/components/products/product-list/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import ProductList from './product-list.styles'
@@ -6,14 +6,9 @@ import OneProduct from '../product'
 import { seedProducts } from '../../../utils/seed-products'
 
 const ProductListComponent = ({ products }) => {
-  useEffect(() => {
-    if (products.length < 1) {
-      seedProducts({ products })
-    }
-  }, [])
-
   return (
     <ProductList>
+      <button onClick={() => seedProducts({ products })}>seed em</button>
       {products.map(p => (
         <OneProduct key={p.id} product={p} />
       ))}
