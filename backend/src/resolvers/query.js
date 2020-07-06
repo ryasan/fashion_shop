d87501612f1a60c211e5a3f91154159b3c5f66b3
Ryan Santos <ryan@ryansantossmbp.home>
2020-07-05 19:05:07 -0700

    refactor: minor change to products connection query

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -18,13 +18,11 @@ const Query = {
     const inStock = edges.filter(({ node }) => {
       return isInStock(node.availableSizes, sizeFilters)
     })
-    const productIds = inStock.map(({ node }) => node.id)
+    const inStockProductIds = inStock.map(({ node }) => node.id)
 
     const where = {
-      AND: [
-        { isFreeShipping: freeShippingSelected || undefined },
-        { id_in: !isEmpty(productIds) ? productIds : undefined }
-      ]
+      ...(freeShippingSelected && { isFreeShipping: true }),
+      ...(!isEmpty(inStockProductIds) && { id_in: inStockProductIds })
     }
 
     return ctx.db.query.productsConnection({ where }, info)
