92a99f224e66df8e47650352bb459c962907bdd0
Ryan Santos <ryan@ryansantossmbp.home>
2020-07-10 19:34:37 -0700

    refactor: minor change to grid layout

--- frontend/src/components/orders/order-list/order-list.styles.js
+++ frontend/src/components/orders/order-list/order-list.styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { device } from '../../../utils'
+
 const Orders = styled.div`
   padding-top: 5rem;
   display: flex;
@@ -8,9 +10,15 @@ const Orders = styled.div`
 
 Orders.List = styled.ul`
   width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  grid-gap: 5rem;
+  justify-items: center;
+  align-items: center;
+  flex: 1;
+  @media ${device.mobileL} {
+    justify-content: center;
+  }
 `
 
 export default Orders
--- frontend/src/components/orders/order/order.styles.js
+++ frontend/src/components/orders/order/order.styles.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 
 import headerBackground from '../../../images/logo-royal-banner.svg'
+import { device } from '../../../utils'
 
 const Order = styled.li`
-  width: 26rem;
+  width: 100%;
   height: 32rem;
   margin-bottom: 4.7rem;
   box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.6);
@@ -15,8 +16,12 @@ Order.Header = styled.div`
   justify-content: center;
   align-items: center;
   background: url(${headerBackground}) center 75%;
+  background-repeat: no-repeat;
   position: relative;
   height: 15rem;
+  @media ${device.mobileL} {
+    background-position-y: 65%;
+  }
   img {
     border-radius: 50%;
     width: 10rem;
