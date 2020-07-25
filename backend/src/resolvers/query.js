c7fc54482153d4d44af4b0a9a8af607a6b06cd90
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-07-24 21:31:51 -0700

    bugfix: debugging current user not being cached on client

--- backend/src/resolvers/query.js
+++ backend/src/resolvers/query.js
@@ -36,6 +36,7 @@ const Query = {
     return ctx.db.query.productsConnection({ where }, info)
   },
   me: (parent, args, ctx, info) => {
+    console.log(ctx.request.userId)
     if (!isLoggedIn(ctx)) return null
     return ctx.db.query.user({ where: { id: ctx.request.userId } }, info)
   },
