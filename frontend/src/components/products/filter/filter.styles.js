7f85f2478efbd998e526c29eb87a0cdf3f254e33
Ryan Santos <ryan@Ryan-Santoss-MacBook-Pro.local>
2020-08-02 03:53:04 -0700

    cleanup: unused filter styles

--- frontend/src/components/products/filter/filter.styles.js
+++ frontend/src/components/products/filter/filter.styles.js
@@ -1,61 +1,7 @@
-import styled, { css } from 'styled-components'
-
-import { Button, P } from '../../../elements'
+import styled from 'styled-components'
 
 const Filter = styled.div`
   position: relative;
 `
 
-const filterBtnStyles = isSelected => css`
-  background: ${isSelected ? 'var(--red)' : 'var(--dark)'};
-  color: ${isSelected ? 'white' : 'white'};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${isSelected ? 'var(--red)' : 'white'};
-  margin-left: 1rem;
-  font-size: var(--font-size-s);
-  outline: none;
-  cursor: pointer;
-  &:hover {
-    background: ${isSelected ? '#dd0000' : 'var(--darker)'};
-  }
-`
-
-Filter.ButtonGroup = styled.div`
-  position: initial;
-`
-
-const WideBox = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-bottom: 1rem;
-`
-
-WideBox.Button = styled(Button)`
-  height: 3.5rem;
-  width: 7rem;
-  ${props => filterBtnStyles(props.isSelected)};
-`
-
-const Round = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-Round.Button = styled(Button)`
-  width: 4rem;
-  height: 4rem;
-  border-radius: 50%;
-  cursor: pointer;
-  ${props => filterBtnStyles(props.isSelected)};
-`
-
-Filter.Title = styled(P)`
-  margin: 0;
-`
-
-export { Round, WideBox }
 export default Filter
--- frontend/src/components/products/filter/index.js
+++ frontend/src/components/products/filter/index.js
@@ -1,79 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { capitalCase } from 'change-case'
+import React from 'react'
 
-import Filter, { Round, WideBox } from './filter.styles'
+import Filter from './filter.styles'
 import MultiLevelDropdown from '../../muli-level-dropdown'
 import {
   useFiltersQuery,
   useAddSizeFilterMutation,
   useRemoveSizeFilterMutation,
-  useToggleFreeShippingMutation,
   useAddCategoryFilterMutation,
   useRemoveCategoryFilterMutation
 } from '../../../graphql/filter/hooks'
 import CATEGORY_FILTERS from '../../../types/category-types.js'
 import SIZE_FILTERS from '../../../types/size-types.js'
 
-const RoundButton = ({ size }) => {
-  const [isSelected, setIsSelected] = useState(false)
-  const [addSizeFilter] = useAddSizeFilterMutation()
-  const [removeSizeFilter] = useRemoveSizeFilterMutation()
-
-  const toggleSelect = () => {
-    setIsSelected(prevState => !prevState)
-  }
-
-  useEffect(() => {
-    const setSizeFilters = isSelected ? addSizeFilter : removeSizeFilter
-    setSizeFilters({ variables: { size } })
-  }, [isSelected])
-
-  return (
-    <Round.Button onClick={toggleSelect} isSelected={isSelected}>
-      {size}
-    </Round.Button>
-  )
-}
-
-const WideButton = ({ category }) => {
-  const [isSelected, setIsSelected] = useState(false)
-  const [addCategoryFilter] = useAddCategoryFilterMutation()
-  const [removeCategoryFilter] = useRemoveCategoryFilterMutation()
-
-  const toggleSelect = () => {
-    setIsSelected(prevState => !prevState)
-  }
-
-  useEffect(() => {
-    const setCategoryFilters = isSelected
-      ? addCategoryFilter
-      : removeCategoryFilter
-    setCategoryFilters({ variables: { category } })
-  }, [isSelected])
-
-  return (
-    <WideBox.Button onClick={toggleSelect} isSelected={isSelected}>
-      {capitalCase(category)}
-    </WideBox.Button>
-  )
-}
+// TODO: add free shipping filter
 
 const FilterComponent = () => {
-  const [isSelected, setIsSelected] = useState(false)
-  const [setFreeShippingSelected] = useToggleFreeShippingMutation()
   const { data: { sizeFilters, categoryFilters } } = useFiltersQuery() // prettier-ignore
   const [addCategoryFilter] = useAddCategoryFilterMutation()
   const [removeCategoryFilter] = useRemoveCategoryFilterMutation()
   const [addSizeFilter] = useAddSizeFilterMutation()
   const [removeSizeFilter] = useRemoveSizeFilterMutation()
-
-  useEffect(() => {
-    setFreeShippingSelected({ variables: { isSelected } })
-  }, [isSelected])
-
-  const toggleFreeShippingSelected = () => {
-    setIsSelected(prevState => !prevState)
-  }
 
   const onCategoryClick = i => {
     const category = CATEGORY_FILTERS[i]
